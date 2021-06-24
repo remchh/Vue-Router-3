937b708f08d0068c8bd37b2d99a7f6b293d0cdc4
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-06-23 22:53:18 -0600

    “Programatic-Navigation”

---
+++ src/views/event/Register.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Registration form here</p>
+    <button @click="register">Register Me!</button>
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  methods: {
+    register() {
+      // If registration API call is successful
+      // Push back to the event details
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
+}
+</script>
