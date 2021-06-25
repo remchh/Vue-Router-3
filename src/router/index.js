ca852692e5e3b7b86d6738369eb9b3c4fc0209e5
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-06-25 13:35:21 -0600

    “Error-Handling”

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import EventDetails from '../views/event/Details.vue'
 import EventRegister from '../views/event/Register.vue'
 import EventEdit from '../views/event/Edit.vue'
 import About from '../views/About.vue'
+import NotFound from '@/views/NotFound.vue'
+import NetworkError from '@/views/NetworkError.vue'
 
 const routes = [
   {
@@ -53,6 +55,22 @@ const routes = [
     name: 'about',
     component: About,
     alias: '/about' // <-----
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError
   }
 ]
 
