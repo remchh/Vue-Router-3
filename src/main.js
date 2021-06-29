0782baa2423019f36c05c9613cd7be76b6671ee3
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-06-28 22:37:11 -0600

    “Flash-Messages”

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'  // <--- importing reactive
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Create a reactive object
+const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore)  // provide this object so others can inject it
   .mount('#app')
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,9 +6,17 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'], // <---- Inject the Global Store
   methods: {
     register() {
       // If registration API call is successful
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {  // After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+
       // Push back to the event details
       this.$router.push({
         name: 'EventDetails',
